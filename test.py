4d44e7c76d43fcd8a96945e204c41d0635c346d0
Toulousegg <franklin.m.mogollon@gmail.com>
2026-02-07 23:35:38 -0300

    delete test

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-header = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NzA0MjY3NDcsInN1YiI6IjMifQ.nsgiUIDK6lDOpnf0JlI37Re1GyHHZtmxh8KrrCy4PNA"
-}
-
-response = requests.get("http://127.0.0.1:8000/home/refresh", headers=header)
-print(response.json())
